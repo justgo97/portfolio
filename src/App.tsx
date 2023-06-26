f541b879a4d5fca76193333979beb7b83f0e47f4
hamdi2050 <hamdi.mar20@gmail.com>
2023-06-26 10:23:37 +0100

    Update footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import reactLogo from "/react.svg";
+
 import "./styles/App.scss";
 
 // Import all of Bootstrap's JS
@@ -43,7 +45,32 @@ function App() {
         <Skills darkMode={darkMode} />
         <Projects />
         <Contact darkMode={darkMode} />
-        <footer>Coded with ❤️</footer>
+        <footer className="fw-bold">
+          <div>
+            Made with{" "}
+            <img
+              src={reactLogo}
+              className="logo react"
+              alt="React logo"
+              style={{ height: "25px" }}
+            />
+          </div>
+          <br />
+          <div>
+            <a
+              className="text-decoration-none text-reset fs-1"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/justgo97/portfolio"
+            >
+              <i
+                className={`devicon-github-original-wordmark ${
+                  !darkMode ? "colored" : ""
+                }`}
+              ></i>
+            </a>
+          </div>
+        </footer>
       </div>
     </main>
   );
